a577f27895a25a3e0b5bd1525c9fab065f99ad94
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-09-03 14:49:14 -0400

    Update

--- app/utils/db.py
+++ app/utils/db.py
@@ -6,35 +6,39 @@ import pprint
 from dotenv import load_dotenv, find_dotenv
 from pymongo import MongoClient
 from urllib.parse import quote_plus
-from utils.encryption_utils import encrypt_email, decrypt_email
+from .encryption_utils import encrypt_email, decrypt_email
 
-# Load .env variables
-load_dotenv(find_dotenv())
 
-# Variables
-username = os.getenv('MONGODB_UID')
-cluster = os.getenv('MONGODB_CLUSTER_NAME')
-authSource = os.getenv('MONGODB_AUTH')
-password = quote_plus(os.getenv('MONGODB_PWD'))
-
-# URI string
-uri = username + ':' + password + '@' + cluster + authSource
-
-# Initialize MongoDB client
-client = MongoClient(uri)
-
-# Update later in production environment ---------------------------------- <<<
-# Connect to database
-users_db = client.test
-# ------------------------------------------------------------------------- <<<
-
-# Define your collections
-users_collection = users_db.users
 
 # Functions for interacting with MongoDB
+def init_db():
+    # Load .env variables
+    load_dotenv(find_dotenv())
+
+    # Variables
+    username = os.getenv('MONGODB_UID')
+    cluster = os.getenv('MONGODB_CLUSTER_NAME')
+    authSource = os.getenv('MONGODB_AUTH')
+    password = quote_plus(os.getenv('MONGODB_PWD'))
+
+    # URI string
+    uri = username + ':' + password + '@' + cluster + authSource
+
+    # Initialize MongoDB client
+    client = MongoClient(uri)
+
+    # Update later in production environment ---------------------------------- <<<
+    # Connect to database
+    users_db = client.test
+    # ------------------------------------------------------------------------- <<<
+
+    # Define your collections
+    return users_db.users
+
 
 def find_user(user_email):
 
+    users_collection = init_db()
     users = users_collection.find()
 
     for user in users:
@@ -48,6 +52,8 @@ def find_user(user_email):
     return False
 
 def insert_user(user_email):
+    users_collection = init_db()
+
     # Check if user already exists
     if find_user(user_email):
         print("User already exists!")
@@ -66,14 +72,17 @@ def insert_user(user_email):
     return result.inserted_id
 
 def update_user(user_email, updated_data):
+    users_collection = init_db()
     # Update user data by email
     return users_collection.update_one({"_id": user_email}, {"$set": updated_data})
 
 def delete_user(user_email):
+    users_collection = init_db()
     # Delete a user by email
     return users_collection.delete_one({"_id": user_email})
 
 def insert_user_response(user_id, responses):
+    users_collection = init_db()
     from bson.objectid import ObjectId
     _id = ObjectId(user_id)
 
@@ -81,6 +90,11 @@ def insert_user_response(user_id, responses):
 
     update = {"$set": {"responses": responses}}
     result = users_collection.update_one(query, update)
+
+    if result:
+        print("success!")
+    else:
+        print("failed!")
 
 
 # Add more functions as needed
@@ -91,7 +105,7 @@ def insert_user_response(user_id, responses):
 user_id = "66bf718f19aab530b26cd132"
 
 responses = {
-    "question_1": "choice_a",
+    "question_1": "choice_d",
     "question_2": "choice_d",
 }
 
